ede4ceb9a83fecb348881af6793ac5c832f66a0e
John Kelly <mr.johnkelly@live.com>
2014-04-21 01:12:22 -0700

    Remove gravatar from serializer

--- spec/serializers/current_user_serializer.rb
+++ spec/serializers/current_user_serializer.rb
@@ -1,14 +1,4 @@
 require 'spec_helper'
 
 describe CurrentUserSerializer do
-  let(:user) { FactoryGirl.build_stubbed_uuid(:user) }
-  let(:serializer) { CurrentUserSerializer.new(user) }
-
-  describe "gravatar" do
-    before do
-      allow(Digest::MD5).to receive(:hexdigest).and_return("camel")
-      expect(user_serializer.gravatar).to eq("https://secure.gravatar.com/avatar/camel?d=identicon")
-    end
-    it { expect(Digest::MD5).to have_received(:hexdigest) }
-  end
 end
