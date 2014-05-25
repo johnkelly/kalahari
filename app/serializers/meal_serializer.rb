7135fcc60029bb98104e6a36dc97bdac2265e1e3
John Kelly <mr.johnkelly@live.com>
2014-05-24 20:50:14 -0700

    Add number of ingredients to meal serializer

--- app/serializers/meal_serializer.rb
+++ app/serializers/meal_serializer.rb
@@ -3,5 +3,9 @@ class MealSerializer < ActiveModel::Serializer
   has_many :ingredients
   has_many :appliances
 
-  attributes :id, :name, :description, :duration
+  attributes :id, :name, :description, :duration, :ingredient_count
+
+  def ingredient_count
+    object.ingredients.count
+  end
 end
